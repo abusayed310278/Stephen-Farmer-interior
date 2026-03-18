f3aa3148df40945aec0283cf1c3bc4079a98715e
sr_sumanroy <sumantachtg@gmail.com>
2026-03-18 09:18:45 +0600

    nvf

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.stephen_farmer"
+        applicationId = "com.nvf.stephenfarmeru"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
